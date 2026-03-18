08c5d60f6bd96ce89e7235b8cce492fcd647b315
nxhung <khongthichdungchung@gmail.com>
2026-03-18 16:48:19 +0700

    fix(middleware): optimize matcher and avoid blocking static assets

--- middleware.ts
+++ middleware.ts
@@ -40,5 +40,5 @@ export default auth((req) => {
 });
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|marketing).*)"],
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico|login|marketing|.*\\.jpg|.*\\.png|.*\\.svg).*)"],
 };
